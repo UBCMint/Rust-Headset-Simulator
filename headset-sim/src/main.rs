5de957f103e59d5e3d388332d3c8c1a6057cb60e
rkna02 <ruikangna0605@hotmail.com>
2024-04-08 21:42:44 -0400

    rust simulator

--- headset-sim/src/main.rs
+++ headset-sim/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rand::Rng;
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
+use serde_json::to_string;
+
+/// WebSocket server
+fn main () {
+    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let random_numbers: Vec<i32> = (0..64).map(|_| rand::thread_rng().gen_range(1..101)).collect();
+                let serialized_numbers = to_string(&random_numbers).unwrap();
+                websocket.send(tungstenite::Message::Text(serialized_numbers)).unwrap();
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+        });
+    }
 }
